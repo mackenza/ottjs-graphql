787a290ff59068b566631880a78d4ad2b50d566f
mackenza <a.mackenzie@gmail.com>
2016-02-07 20:37:17 -0500

    getting static web page demo working

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import graphqlHTTP from 'express-graphql';
 import Schema from './schema/github';
 
 const app = express();
-
+app.use(express.static('public'));
 app.use('/', graphqlHTTP({ schema: Schema, graphiql: true }));
+app.use('/gh', graphqlHTTP({ schema: Schema, graphiql: false }));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
